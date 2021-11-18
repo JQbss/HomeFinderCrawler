1aef4697b295254ca35238571e3dff3e1510abfc
Patryk Wawrzonkowski <wgpol098@gmail.com>
2021-11-18 17:55:22 +0100

    Initial commit

---
+++ DatabaseManager/DbSql.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DatabaseManager
+{
+    public class DbSql
+    {
+        private string _dataSourceString = String.Empty;
+        SqliteConnection _sqlConnection;
+        public DbSql(string DataSourceString)
+        {
+            _dataSourceString = DataSourceString;
+            _sqlConnection = new SqliteConnection(_dataSourceString);
+        }
+
+        public void Connect()
+        {
+            if(_sqlConnection is not null)
+            {
+                _sqlConnection.Open();
+            }
+        }
+
+        public string GetVerson()
+        {
+            var command = _sqlConnection.CreateCommand();
+            command.CommandText = @"select sqlite_version()";
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var name = reader.GetString(0);
+
+                    return ($"SQLite version: {name}!");
+                }
+            }
+
+            return String.Empty;
+        }
+
+        //TODO: Return table list
+        public string GetTablesName()
+        {
+            var command = _sqlConnection.CreateCommand();
+            command.CommandText = @"select distinct t.name as tbl_name
+                                    from sqlite_master AS t
+                                    where t.type = 'table'; ";
+
+            string tablesName = string.Empty;
+            using var reader = command.ExecuteReader();
+            while (reader.HasRows)
+            {
+                while (reader.Read())
+                    tablesName += reader.GetString(0) + Environment.NewLine;
+
+                reader.NextResult();
+            }
+            return tablesName;
+        }
+
+        //TODO
+        public string GetColumnsName(string tableName)
+        {
+            return string.Empty;
+        }
+
+        //TODO: Test
+        public SqliteDataReader ExecuteQuery(string queryString)
+        {
+            var command = _sqlConnection.CreateCommand();
+            command.CommandText = queryString;
+            return command.ExecuteReader();
+        }
+
+        //TODO: Universal columns show
+        public string ShowQuery(string queryString)
+        {
+            if (queryString is null) return string.Empty;
+
+            using SqliteDataReader reader = ExecuteQuery(queryString);
+            string tablesName = string.Empty;
+            while (reader.HasRows)
+            {
+                while (reader.Read())
+                    tablesName += reader.GetString(0) + Environment.NewLine;
+                reader.NextResult();
+            }
+            return tablesName;
+
+        }
+
+        public void CreateMigration()
+        {
+            //TODO: Migration from file
+            string createAnnouncementTable = @"create table  announcement (
+                                               id integer primary key,
+                                               describe text not null);";
+
+            string createImagesTable = @"create table images (
+                                         id integer primaty key, 
+                                         announcement_id id integer, 
+                                         url text);";
+
+
+            ExecuteQuery(createAnnouncementTable);
+            ExecuteQuery(createImagesTable);
+        }
+
+        public void Close()
+        {
+            if(_sqlConnection is not null )
+            {
+                _sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ HomeFinderCrawler/Program.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseManager;
+
+namespace HomeFinderCrawler // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DbSql database = new DbSql("Data Source=:memory:");
+            database.Connect();
+            database.CreateMigration();
+            Console.WriteLine(database.GetVerson());
+            Console.WriteLine(database.GetTablesName());
+
+        }
+    }
+}
